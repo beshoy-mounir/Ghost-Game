a08011a81f29cca59294519df36a994ab9519110
Beshoy <beshoy_mounir22@yahoo.com>
2024-09-02 02:44:27 +0300

    second commit Added Coins & boms "No clean Code :(" but works :)

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
+// Storage
+let score = 0; //current Score
+let highestScore = localStorage.getItem(`HS`);
+// Print Scores
+displayContainer.innerHTML = `<h2>Current Score : ${score}  ||  Highest  Score : ${highestScore}</h2>`;
 // Snake Head
 let snakePointer = document.getElementById("snakeHead");
+
 // counter
-let l = 0;
-let t = 0;
-// Step
-let step = 10;
+let l = 0; //left
+let t = 0; //top
+let x, y; // coin x & y
+let step = 10; // Step
+
+// Generate Coins
+function generateCoinsBoms() {
+    // generate Rundom Num between 0 to  100 and are divided by 5
+    x = Math.floor(Math.random() * 11) * 10;
+    y = Math.floor(Math.random() * 11) * 10;
+    // coin cordanates
+    coin.style.left = `${x}%`;
+    coin.style.top = `${y}%`;
+    coin.style.transform = `translate(-${x}%,-${y}%)`;
+    // generateCoinsBomsBoms
+    boms.style.left = `${100 - x}%`;
+    boms.style.top = `${100 - y}%`;
+    boms.style.transform = `translate(-${100 - x}%,-${100 - y}%)`;
+}
+generateCoinsBoms();
+
+// update Score
+function updateScore() {
+    score++;
+    if (score > highestScore) {
+        highestScore = score;
+        localStorage.setItem(`HS`, `${highestScore}`);
+    }
+    displayContainer.innerHTML = `<h2>Current Score : ${score}  ||  Highest  Score : ${highestScore}</h2>`;
+}
+
 addEventListener("keyup", (i) => {
     // left
     if (i.keyCode === 37) {
@@ -49,5 +82,18 @@ addEventListener("keyup", (i) => {
             snakePointer.style.transform = `translate(-${l}%,-${t}%)`;
             console.log(t);
         }
+    }
+    if (t == y && l == x) {
+        generateCoinsBoms();
+        updateScore();
+    }
+    if (t == 100 - y && l == 100 - x) {
+        l = t = 0;
+        snakePointer.style.top = `0%`;
+        snakePointer.style.left = `0%`;
+        snakePointer.style.transform = `translate(0%,0%)`;
+        generateCoinsBoms();
+        score = 0;
+        displayContainer.innerHTML = `<h2>Current Score : ${score}  ||  Highest  Score : ${highestScore}</h2>`;
     }
 });
